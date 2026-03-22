11f65c40b26417084f1b5307915c986ebe077628
Garima Swami <garimaswami646@gmail.com>
2026-03-22 18:21:09 +0100

    Add files via upload

---
+++ PalindromeNumber.py
@@ -0,0 +1,30 @@
+
+# x=int(input("enter the number: "))
+
+# if str(x)==str(x)[::-1]:
+#     print("palindrome number")
+# else:
+#     print("not palindrome")
+    
+class PalindromeNumber(object):
+    def isPalindrome(self,x):
+        if (x<0):
+            return False
+        
+        original=x
+        rev=0
+
+        while (x>0):
+            digit=x%10
+            rev=rev*10+digit
+            x=x//10
+        
+        return original == rev
+    
+if __name__=="__main__":
+    x=int(input("enter the number :  "))
+
+    sol=PalindromeNumber()
+    result=sol.isPalindrome(x)
+
+    print(result)
